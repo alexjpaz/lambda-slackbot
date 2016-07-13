237ef605b0c61b7b8c3b58a516540de976cf7c08
Alexander Paz <alexjpaz@users.noreply.github.com>
2016-07-13 15:43:41 -0400

    Update hide-the-pain.js

--- src/actions/hide-the-pain.js
+++ src/actions/hide-the-pain.js
@@ -4,6 +4,10 @@ module.exports = function(event) {
     "http://i.imgur.com/am3qXZ1.jpg",
     "http://i3.kym-cdn.com/photos/images/newsfeed/000/839/199/8a9.jpg",
     "http://i0.kym-cdn.com/entries/icons/original/000/016/546/5024850.gif",
+    "http://i2.kym-cdn.com/photos/images/masonry/001/119/076/193.jpg",
+    "http://i.imgur.com/MI2Pf2H.jpg?1",
+    "http://i.imgur.com/AHOj2rP.jpg",
+    "http://ci.memecdn.com/379/8119379.jpg",
     "http://ci.memecdn.com/432/7818432.jpg"
   ];
   function getRandomImage() {
