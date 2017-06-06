136e9ec5b220800be44a118fa1eb695fe1b7d340
Alexander Paz <alexjpaz@users.noreply.github.com>
2017-06-06 14:30:40 -0400

    Update heck.js

--- src/actions/heck.js
+++ src/actions/heck.js
@@ -6,7 +6,14 @@ const images = [
   "http://i.imgur.com/s5T8V6f.jpg",
   "http://i.imgur.com/n9ODwnC.jpg",
   "http://i.imgur.com/0b6cKVP.jpg",
-  "http://i.imgur.com/OZgZNRY.jpg"
+  "http://i.imgur.com/OZgZNRY.jpg",
+  "http://i.imgur.com/8KaC8b4.jpg",
+  "http://i.imgur.com/ZHYkp1C.jpg",
+  "http://i.imgur.com/0W2M6tR.jpg",
+  "http://i.imgur.com/H80fcZP.jpg",
+  "http://i.imgur.com/BQN6191.jpg",
+  "http://i.imgur.com/nFZQQyS.jpg",
+  "http://i.imgur.com/H0EtSBi.jpg"
 ];
 
 module.exports = function(event) {
